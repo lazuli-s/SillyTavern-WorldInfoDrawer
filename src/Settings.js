2db4a2b694d5f9a38cb05486bdadd0c1d970d723
lazuli-s <81201444+lazuli-s@users.noreply.github.com>
2025-12-26 21:15:15 -0300

    Fix parse script compatibility

--- src/Settings.js
+++ src/Settings.js
@@ -1,78 +1,90 @@
-import { saveSettingsDebounced } from '../../../../../script.js';
-import { extension_settings } from '../../../../extensions.js';
+(async () => {
+    const [
+        { saveSettingsDebounced },
+        { extension_settings },
+    ] = await Promise.all([
+        import('../../../../../script.js'),
+        import('../../../../extensions.js'),
+    ]);
 
+    /** @readonly */
+    /** @enum {string} */
+    const SORT = {
+        /** Alphabetical by entry title */
+        TITLE: 'title',
+        /** Numeric position value */
+        POSITION: 'position',
+        /** Numeric depth value */
+        DEPTH: 'depth',
+        /** By numeric order value */
+        ORDER: 'order',
+        /** By numeric UID */
+        UID: 'uid',
+        /** Alphabetical by trigger/keywords */
+        TRIGGER: 'trigger',
+        /** By token/word count */
+        LENGTH: 'length',
+        /** Alphabetical by entry comment (title/memo) */
+        ALPHABETICAL: 'alphabetical',
+        /** According to prompt depth (position-depth-order) */
+        PROMPT: 'prompt',
+    };
+    /** @readonly */
+    /** @enum {string} */
+    const SORT_DIRECTION = {
+        /** Alphabetical by entry comment (title/memo) */
+        ASCENDING: 'ascending',
+        /** According to prompt depth (position-depth-order) */
+        DESCENDING: 'descending',
+    };
 
-/** @readonly */
-/** @enum {string} */
-export const SORT = {
-    /** Alphabetical by entry title */
-    TITLE: 'title',
-    /** Numeric position value */
-    POSITION: 'position',
-    /** Numeric depth value */
-    DEPTH: 'depth',
-    /** By numeric order value */
-    ORDER: 'order',
-    /** By numeric UID */
-    UID: 'uid',
-    /** Alphabetical by trigger/keywords */
-    TRIGGER: 'trigger',
-    /** By token/word count */
-    LENGTH: 'length',
-    /** Alphabetical by entry comment (title/memo) */
-    ALPHABETICAL: 'alphabetical',
-    /** According to prompt depth (position-depth-order) */
-    PROMPT: 'prompt',
-};
-/** @readonly */
-/** @enum {string} */
-export const SORT_DIRECTION = {
-    /** Alphabetical by entry comment (title/memo) */
-    ASCENDING: 'ascending',
-    /** According to prompt depth (position-depth-order) */
-    DESCENDING: 'descending',
-};
-
-export class Settings {
-    /**@type {Settings} */
-    static #instance;
-    static get instance() {
-        if (!this.#instance) {
-            this.#instance = new Settings();
+    class Settings {
+        /**@type {Settings} */
+        static #instance;
+        static get instance() {
+            if (!this.#instance) {
+                this.#instance = new Settings();
+            }
+            return this.#instance;
         }
-        return this.#instance;
-    }
-    /**@type {SORT} */
-    sortLogic = SORT.TITLE;
-    /**@type {SORT_DIRECTION} */
-    sortDirection = SORT_DIRECTION.ASCENDING;
-    /**@type {boolean} */
-    useBookSorts = true;
+        /**@type {SORT} */
+        sortLogic = SORT.TITLE;
+        /**@type {SORT_DIRECTION} */
+        sortDirection = SORT_DIRECTION.ASCENDING;
+        /**@type {boolean} */
+        useBookSorts = true;
 
-    constructor() {
-        Object.assign(this, extension_settings.wordInfoDrawer ?? {});
-        extension_settings.wordInfoDrawer = this;
+        constructor() {
+            Object.assign(this, extension_settings.wordInfoDrawer ?? {});
+            extension_settings.wordInfoDrawer = this;
 
-        if (!Object.values(SORT).includes(this.sortLogic)) {
-            this.sortLogic = SORT.TITLE;
+            if (!Object.values(SORT).includes(this.sortLogic)) {
+                this.sortLogic = SORT.TITLE;
+            }
+            if (!Object.values(SORT_DIRECTION).includes(this.sortDirection)) {
+                this.sortDirection = SORT_DIRECTION.ASCENDING;
+            }
+            if (typeof this.useBookSorts !== 'boolean') {
+                this.useBookSorts = true;
+            }
         }
-        if (!Object.values(SORT_DIRECTION).includes(this.sortDirection)) {
-            this.sortDirection = SORT_DIRECTION.ASCENDING;
+
+        toJSON() {
+            return {
+                sortLogic: this.sortLogic,
+                sortDirection: this.sortDirection,
+                useBookSorts: this.useBookSorts,
+            };
         }
-        if (typeof this.useBookSorts !== 'boolean') {
-            this.useBookSorts = true;
+
+        save() {
+            saveSettingsDebounced();
         }
     }
 
-    toJSON() {
-        return {
-            sortLogic: this.sortLogic,
-            sortDirection: this.sortDirection,
-            useBookSorts: this.useBookSorts,
-        };
-    }
-
-    save() {
-        saveSettingsDebounced();
-    }
-}
+    globalThis.WorldInfoDrawerSettings = {
+        Settings,
+        SORT,
+        SORT_DIRECTION,
+    };
+})();
