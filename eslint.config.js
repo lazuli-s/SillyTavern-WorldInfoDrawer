e75c89295f9fe58dbd99c2ffe136bf6654a6b5aa
lazuli-s <isabellagmdias@gmail.com>
2026-02-06 00:35:32 -0300

    chore(eslint): add flat config with globals and base rules

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        ignores: ['node_modules/**', 'vendor/**', 'coverage/**'],
+        files: ['**/*.js'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: {
+                $: 'readonly',
+                toastr: 'readonly',
+                hljs: 'readonly',
+            },
+        },
+        rules: {
+            'no-undef': 'error',
+            'no-unused-vars': ['error', { argsIgnorePattern: '^(_|evt)$' }],
+            'no-unreachable': 'error',
+            eqeqeq: 'warn',
+            'no-redeclare': 'error',
+            'no-shadow': 'warn',
+        },
+    },
+];
